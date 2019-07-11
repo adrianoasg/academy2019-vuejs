edb3870d9b40bc23654089d709d99e7d0efc3b05
adriano <adrianoasg@hotmail.com>
2019-07-11 20:57:42 -0300

    fundamentos vue

--- 1-javascript-fundamentos/7-objetos/js/main.js
+++ 1-javascript-fundamentos/7-objetos/js/main.js
@@ -1,14 +1,45 @@
-const objeto = {
-  nome: 'Margaret J. Shah',
-  idade: 23,
-  soma: function (a, b) {
-    return a + b
-  },
-  nomeInteiro (nome, sobrenome) {
-    return `${nome} ${sobrenome}`
-  },
-  usuario: {
-    nome: 'Diane M. Bankston',
-    telefone: '843-469-9199',
-  }
+//const objeto = {
+  //nome: 'Margaret J. Shah',
+  //idade: 23,
+  //soma: function (a, b) {
+    //return a + b
+  //},
+  //nomeInteiro (nome, sobrenome) {
+    //return `${nome} ${sobrenome}`
+  //},
+  //usuario: {
+    //nome: 'Diane M. Bankston',
+    //telefone: '843-469-9199',
+  //}
+//}
+
+//Object.freeze(objeto)
+//Object.freeze(objeto.usuario)
+
+//objeto.nome = 'Breno'
+
+//console.log(objeto) 
+
+
+const produto = {
+  descricao: 'Pastel',
+  recheio: 'Queijo',
+  peso: '100g',
+  preco: 5.99,
+
+  valorPedido: function (qtd) {
+    return this.preco * qtd
+  }  
+  
 }
+
+
+
+produto.descricao = 'Coxinha'
+produto.recheio = 'frango'
+
+console.log(produto)
+
+console.log(produto.valorPedido(3))
+
+console.log(produto.descricao)
--- 1-javascript-fundamentos/8-arrays/js/main.js
+++ 1-javascript-fundamentos/8-arrays/js/main.js
@@ -1,53 +1,123 @@
-const numeros = [10, 5, 3, 3, 2, 9]
-const pessoas = [
-  {
-    nome: 'Christie C. Craighead',
-    idade: 15
-  },
-  {
-    nome: 'Arlene T. Baker',
-    idade: 14
-  },
-  {
-    nome: 'Christie C. Craighead',
-    idade: 18
-  },
-  {
-    nome: 'Scott K. Parham',
-    idade: 23
-  },
-  {
-    nome: 'Christie C. Craighead',
-    idade: 30
-  },
-  {
-    nome: 'Jeremiah J. Lobel',
-    idade: 8
-  },
-  {
-    nome: 'Deborah W. Coulter',
-    idade: 21
-  },
-]
-const salas = ['A', 'B', 'C', 'D', 'E']
+//const numeros = [10, 5, 3, 3, 2, 9]
+//const pessoas = [
+  //{
+    //nome: 'Christie C. Craighead',
+    //idade: 15
+  //},
+  //{
+    //nome: 'Arlene T. Baker',
+    //idade: 14
+  //},
+  //{
+   // nome: 'Christie C. Craighead',
+    //idade: 18
+ // },
+  //{
+    //nome: 'Scott K. Parham',
+    //idade: 23
+  //},
+  //{
+    //nome: 'Christie C. Craighead',
+    //idade: 30
+  //},
+  //{
+    //nome: 'Jeremiah J. Lobel',
+    //idade: 8
+  //},
+  //{
+  //  nome: 'Deborah W. Coulter',
+//    idade: 21
+  //},
+//]
+//const salas = ['A', 'B', 'C', 'D', 'E']
 
 // map
 
-const alunosSalas = pessoas.map(pessoa => {
-  return {
-    ...pessoa,
-    sala: salas[Math.floor(Math.random()*salas.length)]
-  }
-})
+//const alunosSalas = pessoas.map(pessoa => {
+  //const teste = { a: 3434, b: 49878}
+  //return { 
+    //...pessoa,
+    //a: teste.a,
+    //sala: salas[Math.floor(Math.random()*salas.length)]
+  //}
+//})
+
+//console.log('novo array' ,alunosSalas)
 
 // filter
 
-const alunosMaioresDeIdade = pessoas.filter(pessoa => {
-  return pessoa.idade >= 18
-})
+//const alunosMaioresDeIdade = pessoas.filter(pessoa => {
+  //return pessoa.idade >= 18
+//})
+
+//console.log(alunosMaioresDeIdade)
 
 // reduce
+//const valorInicial = 0
+//const somaDosNumeros = numeros.reduce((acumulador, valorAtual) => {
+  //return acumulador + valorAtual
+//}, valorInicial)
+
+//console.log(somaDosNumeros)
+
+//const debora = pessoas.find(pessoa.nome === 'Deborah W. Coulter')
+
+//console.log(debora)
+
+
+
+const nomes = ['Rafael', 'Maria', 'Marta', 'Jaqueline', 'Bruno']
+
+const carros = [
+  {
+    modelo: 'Fusca',
+    ano: '1976',
+    cor: 'branco',
+    valor: 9.000
+  },
+  {
+    modelo: 'Vectra',
+    ano: '2002',
+    cor: 'prata',
+    valor: 18.000
+
+  },
+  {
+  modelo: 'Uno',
+  ano: '1997',
+  cor: 'verde',
+  valor: 12.000 
+  },
+  {
+  modelo: 'Mustang',
+  ano: '1966',
+  cor: 'preto',
+  valor: 70.000
+  }
+]
+
+const saloes = ['Lançamentos', 'Populares', 'Classicos']
+
+const salaoAutomovel = carros.map(carro => {
+  return { 
+    ...carro,
+    salao: saloes[Math.floor(Math.random()*saloes.length)]
+  }
+})
+
+console.log(salaoAutomovel)
+
+const carrosValorMaisAlto = carros.filter(carro => {
+  return carro.valor >= 12.000
+})
+
+
+console.log(carrosValorMaisAlto)
+
+
 const valorInicial = 0
-const somaDosNumeros = numeros.reduce((acumulador, valorAtual) => {
-  return acumulador + valorAtual
+const somaDosPrecos = carros.reduce((acumulador, valorAtual) => {
+  return acumulador + valorAtual.valor
 }, valorInicial)
+
+console.log(somaDosPrecos)
